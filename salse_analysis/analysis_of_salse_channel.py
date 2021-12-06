768c9d259e15fcbeef3ee1fc12c92294b8371c95
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-12-06 11:31:02 +0900

    エクセルファイルへの書き込み

--- salse_analysis/analysis_of_salse_channel.py
+++ salse_analysis/analysis_of_salse_channel.py
@@ -9,8 +9,14 @@ for excel_file in excel_files:
         list_sales_data.append(sales_data)
 
 sales_channel = pd.read_excel(folder_path+'取引先流入元.xlsx')
-sales_summary = pd.concat(list_sales_data, ignore_index = True)
+sales_summary = pd.concat(list_sales_data, ignore_index=True)
 sales_summary = pd.marge(sales_channel, sales_summary, on='取引先名')
+sales_by_channel = sales_summary.groupby('流入元').sum()
+print(sales_by_channel)
+
+with pd.ExcelWriter('summary.xlsx')as writer:
+    sales_summary.to_excel(writer, sheet_name='売上サマリー')
+    sales_by_channel.to_excel(writer, sheet_name='流入ごとの売上')
 
 
 
