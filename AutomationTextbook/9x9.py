7a228ab6228c82a5e981505958391de33203de2e
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-12-11 13:07:16 +0900

    100cellの書き込み

---
+++ AutomationTextbook/9x9.py
@@ -0,0 +1,10 @@
+import openpyxl as excel
+book = excel.Workbook()
+sheet = book.active
+
+for y in range(1, 10):
+    for x in range(1, 10):
+        cell = sheet.cell(row=y, column=x)
+        cell.value = x * y
+
+book.save("9x9.xlsx")
