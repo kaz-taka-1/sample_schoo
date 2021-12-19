197dd68789f2d20480eb62f618aa7758b93d4a4a
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-12-19 14:11:07 +0900

    uriage_get_all4の追加

--- AutomationTextbook/uriage_get_all4.py
+++ AutomationTextbook/uriage_get_all4.py
@@ -1,9 +1,9 @@
 import openpyxl as excel
-
-book = excel.load_workbook("uriage.xlsx", data_only=True)
+book = excel .load_workbook(
+    "uriage.xlsx",data_only=True)
 sheet = book.active
-
-rows = sheet["A3":"F9"]
+rows = sheet["A3":"F999"]
 for row in rows:
     values = [cell.value for cell in row]
+    if values[0] is None: break
     print(values)
