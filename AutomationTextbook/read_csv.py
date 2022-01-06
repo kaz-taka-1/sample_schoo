7694a8ff0c74c57c074cc9a0d218d0d013061502
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-01-07 00:12:31 +0900

    pythonでcsvを扱う

---
+++ AutomationTextbook/read_csv.py
@@ -0,0 +1,13 @@
+import csv, pprint
+
+with open('items.csv', encoding='sjis') as f:
+    text = f.read().strip()
+    lines = text.split("¥n")
+    data = [v.split(',') for v in lines]
+    pprint.pprint(data)
+
+with open('items.csv', encoding = 'sjis') as f:
+    reader = csv.reader(f)
+    data = [row for row in reader]
+    pprint.pprint(data)
+
