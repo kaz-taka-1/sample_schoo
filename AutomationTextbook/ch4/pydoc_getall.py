5ae8e28437fa540bcce7924c7ec7379f6f9e862c
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-01-25 23:49:22 +0900

    pythonチューとリアlをダウンロードする

---
+++ AutomationTextbook/ch4/pydoc_getall.py
@@ -0,0 +1,30 @@
+import requests, urllib, os, time
+from bs4 import BeautifulSoup
+
+save_dir = './pydoc_tutorial'
+top_page = 'https/:/docs.python.org/ja/3/tutorial/index/html'
+check_url = 'https://docs.python.org/ja/3/tutorial'
+visited = []
+
+def get_page(url):
+    if check_url not in url: return
+    if url in visited: return
+    visited[url] = True
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    html = res.text
+    fname = save_dir + '/' + os.path.basename(url)
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    with open(fname, "wt", encoding="utf-8") as f:
+        f.write(html)
+        print('save:', fname)
+    time.sleep(1)
+    soup = BeautifulSoup(html, 'html5lib')
+    for a in soup.find_all('a'):
+        a_url = urllib.parse.urljoin(url, a['href'])
+        a_url = urllib.parse.urldefrag(a_url)[0]
+        get_page(a_url)
+
+if __name__ == '__main__':
+    get_page(top_page)
