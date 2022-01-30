43b3ae9ffee41c0202c80ddd1bbb6d9c461de0e1
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-01-30 23:48:36 +0900

    ログイン

---
+++ AutomationTextbook/ch4/chrome_login_download.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import os, time
+login_url = 'https://uta.pw/sakusibbs/users.php?action=login'
+user_id, password = ('JS-TESTER', 'ipCU12ySxI')
+save_dir = os.path.dirname(os.path.abspath(__file__))
+save_file = save_dir + '/list.csv'
+options = webdriver.ChromeOptions()
+options.add_experimental_option('prefs',{
+    'download.default_directory': save_dir})
+
+def login_download():
+    driver = webdriver.Chrome(options=options)
+    try_login(driver)
+    link_click(driver,'マイページ')
+    link_click(driver,'CSVでダウンロード')
+    for i in range(30):
+        if os.path.exists(save_file): break
+        time.sleep(1)
+
+def try_login(driver):
+    driver.get(login_url)
+    user = driver.find_element_by_name('username_mm1bbs6')
+    user.send_keys(user_id)
+    pwd = driver.find_element_by_name('password_mm1bbs6')
+    pwd.send_keys(password)
+    pwd.submit()
+
+def link_click(driver, label):
+    a = driver.find_element_by_partial_link_text(label)
+    a.click()
+
+if __name__=='__main__':
+    login_download()
