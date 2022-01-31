3a784cc0432fbc33b2f69211f87866db556f4cbc
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-02-01 01:58:32 +0900

    Requestsを利用してファイルをダウンロード

--- AutomationTextbook/ch4/requests_logun_download.py
+++ AutomationTextbook/ch4/requests_logun_download.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 login_url = 'https://uta.pw/sakusibbs/users.php?' + \
     'action=login&m=try'
-user_id, password = ('JS-TESTER', 'ipCU12ySxl')
+user_id, password = ('JS-TESTER', 'ipCU12ySxI')
 save_file = './list2.csv'
 
 def login_download():
@@ -12,11 +12,21 @@ def login_download():
         'username_mmlbbs6': user_id,
         'password_mmlbbs6': password})
     time.sleep(1)
-    mypage_url = get_link(res.text,'マイページ')
+    mypage_url = get_link(res.text, 'マイページ')
     mypage_html = session.get(mypage_url).text
     time.sleep(1)
-    csv_url = get_link(mypage_html,'CSVでダウンロード')
+    csv_url = get_link(mypage_html, 'CSVでダウンロード')
     download_save(session, csv_url)
+
+def get_link(html, label):
+    soup = BeautifulSoup(html, 'html5lib')
+    for a in soup.find_all('a'):
+        if label in a.text:
+            url = urllib.parse.urljoin(login_url, a['href'])
+            print('url=', url)
+            return url
+        print(label + 'が見つかりません。')
+        quit()
 
 def download_save(session, csv_url):
     res = session.get(csv_url)
