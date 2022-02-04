c8683c8c72c5e537c1941be258a5935663d37e5a
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-02-04 23:51:29 +0900

    gmailのメール送信

---
+++ AutomationTextbook/ch5/gmail_send.py
@@ -0,0 +1,28 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+import my_gmail_account as gmail
+
+def send_test_email():
+    msg = make_mime_text(
+        mail_to=gmail.account,
+        subject='メールの送信テスト',
+        body='こんにちは。届きましたか？テストテスト。')
+    send_gmail(msg)
+
+def make_mime_text(mail_to, subject, body):
+    msg = MIMEText(body, 'html')
+    msg['Subject'] = subject
+    msg['To'] = mail_to
+    msg['From'] = gmail.account
+    return msg
+
+def send_gmail(msg):
+    server = smtplib.SMTP_SSL(
+        'smtp.gmail.com', 465,
+        context=ssl.create_default_context())
+    server.login(gmail.account, gmail.password)
+    server.send_message(msg)
+
+if __name__ == '__main__':
+    send_test_email()
+    print('ok')
