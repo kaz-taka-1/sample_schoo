06accb04a9b2ca5d64cde8ef210228a77040c501
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-02-14 23:45:48 +0900

    Flaskでファイルを共有

---
+++ AutomationTextbook/ch6/share.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import glob
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    files = glob.glob('static/*')
+    html = '<html><meta charset="utf-8"><body>'
+    html += '<h1>ファイル一覧</h1>'
+    for f in files:
+        html +='<p><a href="{0}">{0}</a></p>'.format(f)
+    html += '</body></html>'
+    return html
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
