9e4f9581c4ef9e634623a2a904ad85872348a0cc
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-02-27 23:33:56 +0900

    英語辞書で正規表現

---
+++ AutomationTextbook/ch6/re_dict.py
@@ -0,0 +1,20 @@
+import re
+
+dict_file = './ejdict-hand-utf8.txt'
+
+while True:
+    print('正規表現を入力してください。[p]で終了。')
+    re_str = input()
+    if re_str =='':continue
+    if re_str == 'q':break
+
+    with open(dict_file, 'rt', encoding='utf-8') as f:
+        cnt = 0
+        while True:
+            line = f.readline()
+            if not line: break
+            word,mean = line.split('\t')
+            if re.search(re_str, word):
+                print(word)
+                cnt += 1
+                if cnt > 50: break
