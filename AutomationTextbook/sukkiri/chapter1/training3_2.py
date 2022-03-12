02005c89ff5ec481e82c2fe07d8c7d9069447e8d
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2022-03-12 23:54:28 +0900

    条件分岐２

---
+++ AutomationTextbook/sukkiri/chapter1/training3_2.py
@@ -0,0 +1,13 @@
+word = input('何か入力してください：')
+
+if word == 'こんにちは':
+    print('ようこそ')
+elif word == '景気は？':
+    print('ぼちぼちです')
+elif word == 'さようなら':
+    print('お元気で！')
+else:
+    print('どうしました？')
+
+
+
